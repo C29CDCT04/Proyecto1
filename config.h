4d8a39e2dfad6d34bc04495a8cf4d11b1ffe06d0
Carlos Camacho <cam22690@uvg.edu.gt>
2024-08-25 22:07:37 -0600

    Create config.h
    Librería para el uso de Adafruit IO

---
+++ config.h
@@ -0,0 +1,12 @@
+/************************ Adafruit IO Config *******************************/
+#define IO_USERNAME  "CarlosCamacho"
+#define IO_KEY       "aio_Evfg34AR0uys56FIERBKZjFFepux"
+
+/************************ WIFI *******************************/
+#define WIFI_SSID "CLARO1_4A63F6"
+#define WIFI_PASS "707Y7YtKxH"
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
